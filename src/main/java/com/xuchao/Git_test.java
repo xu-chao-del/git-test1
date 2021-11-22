ad3eda229facc4365f7eba748d27a696f38c7429
yry <yry@qq.com>
2021-11-22 20:40:00 +0800

    hot-fix second

--- src/main/java/com/xuchao/Git_test.java
+++ src/main/java/com/xuchao/Git_test.java
@@ -4,5 +4,6 @@ public class Git_test {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git 4");
+        System.out.println("hot-fix commit");
     }
 }
