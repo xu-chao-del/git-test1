46ea67962503586f0e03d72ce1fa4535b0b8e584
yry <yry@qq.com>
2021-11-22 20:18:49 +0800

    second commit

--- src/main/java/com/xuchao/Git_test.java
+++ src/main/java/com/xuchao/Git_test.java
@@ -3,5 +3,10 @@ package com.xuchao;
 public class Git_test {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git111111111");
+
     }
 }
