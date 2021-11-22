036f16349df5677655e71a6666172e9782d9d0cf
yry <yry@qq.com>
2021-11-22 20:27:45 +0800

    hot-fix first

--- src/main/java/com/xuchao/Git_test.java
+++ src/main/java/com/xuchao/Git_test.java
@@ -3,5 +3,6 @@ package com.xuchao;
 public class Git_test {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git 4");
     }
 }
